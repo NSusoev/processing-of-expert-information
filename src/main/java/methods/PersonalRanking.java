2f01feec7ecb0dc09a274c9232ef5a1370726360
NSusoev <susoev.nikita@gmail.com>
2016-10-17 17:11:22 +0300

    Backbone has been created. #1 closed

---
+++ src/main/java/methods/PersonalRanking.java
@@ -0,0 +1,13 @@
+package methods;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonalRanking {
+   public static List<Number> RankBySecondMethod(List<Number> objects) {
+
+       return objects;
+   }
+}
