b1a721b655bde39d23c5840f200c4a0ecb026290
NSusoev <susoev.nikita@gmail.com>
2016-10-19 00:32:22 +0300

    #2 RankedObject and RankStep model were added
    RankStep is needed for visualization of algorithm step by step

---
+++ src/main/java/app/model/RankStep.java
@@ -0,0 +1,64 @@
+package app.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RankStep {
+
+    // ranking step's number
+    private int stepNumber;
+    // count of comparisons
+    private int comparisonsCount;
+    // state
+    private List<RankedObject> rankedObjectList;
+
+    public RankStep(int stepNumber) {
+        this.stepNumber = stepNumber;
+        rankedObjectList = new ArrayList<>();
+    }
+
+    public RankStep(int stepNumber, int comparisonsCount) {
+        this.stepNumber = stepNumber;
+        this.comparisonsCount = comparisonsCount;
+        rankedObjectList = new ArrayList<>();
+    }
+
+    public RankStep(int stepNumber, int comparisonsCount, List<RankedObject> rankedObjectList) {
+        this.stepNumber = stepNumber;
+        this.comparisonsCount = comparisonsCount;
+        this.rankedObjectList = rankedObjectList;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public int getComparisonsCount() {
+        return comparisonsCount;
+    }
+
+    public void setComparisonsCount(int comparisonsCount) {
+        this.comparisonsCount = comparisonsCount;
+    }
+
+    public List<RankedObject> getRankedObjectList() {
+        return rankedObjectList;
+    }
+
+    public void setRankedObjectList(List<RankedObject> rankedObjectList) {
+        this.rankedObjectList = rankedObjectList;
+    }
+
+    @Override
+    public String toString() {
+        return "\nRankStep{" +
+                "stepNumber=" + stepNumber +
+                ", comparisonsCount=" + comparisonsCount +
+                ", rankedObjectList=" + rankedObjectList +
+                "}";
+    }
+}
---
+++ src/main/java/app/model/RankedObject.java
@@ -0,0 +1,38 @@
+package app.model;
+
+public class RankedObject {
+
+    // object's index in initial list
+    private int initialIndex;
+    // object's weight
+    private float weight;
+
+    public RankedObject(int initialIndex, float weight) {
+        this.initialIndex = initialIndex;
+        this.weight = weight;
+    }
+
+    public int getInitialIndex() {
+        return initialIndex;
+    }
+
+    public void setInitialIndex(int initialIndex) {
+        this.initialIndex = initialIndex;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "RankedObject{" +
+                "initialIndex=" + initialIndex +
+                ", weight=" + weight +
+                '}';
+    }
+}
