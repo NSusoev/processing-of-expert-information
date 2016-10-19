5651146ee6d83e44af19b6bf2e14694abc3de836
NSusoev <susoev.nikita@gmail.com>
2016-10-19 14:28:52 +0300

    #2 Necessary controllers were implemented
    one displays input form and another displays calc result

--- src/main/java/app/controller/AppController.java
+++ src/main/java/app/controller/AppController.java
@@ -1,14 +1,17 @@
 package app.controller;
 
+import app.model.PersonalRankingForm;
 import app.model.RankStep;
 import app.service.PersonalRanking;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,16 +27,34 @@ public class AppController {
     }
 
     @RequestMapping(value = "/second/form")
-    public String viewSecondMethodForm()  {
-        return "index";
+    public String viewSecondMethodForm(@Valid PersonalRankingForm personalRankingForm,
+                                       BindingResult bindingResult)  {
+        return "second_method_form";
     }
 
-    @RequestMapping(value = "/second/calc")
-    public String doSecondMethod(Model model) {
+    @RequestMapping(value = "/second/calc", method = RequestMethod.POST)
+    public String doSecondMethod(@Valid PersonalRankingForm personalRankingForm,
+                                 BindingResult bindingResult,
+                                 Model model) {
         log.debug("ENTER");
-        List<Float> initial = new ArrayList<>(Arrays.asList(10f, 5f, 3f, 4f, 2f, 1f, 6f, 7f, 8f, 9f));
-        List<RankStep> steps = PersonalRanking.RankBySecondMethod(initial);
-        model.addAttribute("initial", initial);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("error", "Введите данные для ранжирования");
+            return "second_method_form";
+        }
+
+        List<Float> sequence = new ArrayList<>();
+        try {
+            for (String val : Arrays.asList(personalRankingForm.getSequence().split(" "))) {
+                sequence.add(Float.parseFloat(val));
+            }
+        } catch (NumberFormatException e) {
+            model.addAttribute("error", "Необходимо ввести только числовые значения");
+            model.addAttribute("sequence", personalRankingForm.getSequence());
+            return "second_method_form";
+        }
+
+        List<RankStep> steps = PersonalRanking.RankBySecondMethod(sequence);
+        model.addAttribute("initial", sequence);
         model.addAttribute("steps", steps);
         log.debug("EXIT");
         return "second_method_result";
