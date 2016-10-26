b761987eb539e4fde10b9d2ecde066770dd4db33
NSusoev <susoev.nikita@gmail.com>
2016-10-26 10:41:57 +0300

    #3 Model for containing sum ranks of alternatives was created

---
+++ src/main/java/app/model/ranking/group/ObjectSumRank.java
@@ -0,0 +1,47 @@
+package app.model.ranking.group;
+
+public class ObjectSumRank implements Comparable {
+    private int objectNumber;
+    private float sumRank;
+
+    public ObjectSumRank(int objectNumber, float sumRank) {
+        this.objectNumber = objectNumber;
+        this.sumRank = sumRank;
+    }
+
+    public int getObjectNumber() {
+        return objectNumber;
+    }
+
+    public void setObjectNumber(int objectNumber) {
+        this.objectNumber = objectNumber;
+    }
+
+    public float getSumRank() {
+        return sumRank;
+    }
+
+    public void setSumRank(float sumRank) {
+        this.sumRank = sumRank;
+    }
+
+    public int compareTo(Object obj) {
+        ObjectSumRank objectSumRank = (ObjectSumRank) obj;
+
+        if (objectSumRank.sumRank > this.sumRank) {
+            return -1;
+        } else if (objectSumRank.sumRank < this.sumRank) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectSumRank{" +
+                "objectNumber=" + objectNumber +
+                ", sumRank=" + sumRank +
+                '}';
+    }
+}
