6fbbf0f9ccf39653475f32267974dc3b9f602dc0
NSusoev <susoev.nikita@gmail.com>
2016-10-26 10:51:52 +0300

    #3 Controllers for Bord's method data input and displaying was implemented

--- src/main/java/app/controller/AppController.java
+++ src/main/java/app/controller/AppController.java
@@ -1,5 +1,8 @@
 package app.controller;
 
+import app.model.ranking.group.BordRankingForm;
+import app.model.ranking.group.BordRankingResult;
+import app.model.ranking.group.Relation;
 import app.model.ranking.personal.PersonalRankingForm;
 import app.model.ranking.personal.RankStep;
 import app.service.PersonalRanking;
@@ -11,10 +14,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class AppController {
@@ -59,4 +64,50 @@ public class AppController {
         log.debug("EXIT");
         return "second_method_result";
     }
+
+    @RequestMapping(value = "/bord/form")
+    public String viewBordMethodForm(@Valid BordRankingForm bordRankingForm,
+                                     BindingResult bindingResult,
+                                     HttpServletRequest request,
+                                     Model model) {
+        log.debug("ENTER");
+        if (Objects.equals(request.getMethod(), "POST")) {
+            log.debug("experts = {}", bordRankingForm.getExpertsCount());
+            log.debug("objects = {}", bordRankingForm.getObjectsCount());
+            model.addAttribute("experts_count", bordRankingForm.getExpertsCount());
+            model.addAttribute("objects_count", bordRankingForm.getObjectsCount());
+
+            if (bindingResult.hasErrors()) {
+                model.addAttribute("error", "Проверьте введенные вами данные");
+                return "bord_method_form";
+            } else {
+                model.addAttribute("bord_result", 1);
+                model.addAttribute("relations", Relation.values());
+                return "bord_method_form";
+            }
+        }
+        log.debug("EXIT");
+        return "bord_method_form";
+    }
+
+    @RequestMapping(value = "/bord/calc", method = RequestMethod.POST)
+    public String viewBordInputDataForm(@Valid BordRankingForm bordRankingForm,
+                                        BindingResult bindingResult,
+                                        Model model) {
+        log.debug("ENTER");
+        log.debug("form data = {}", bordRankingForm);
+        for (int i = 0; i < bordRankingForm.getPersonalRankings().size(); i++) {
+            bordRankingForm.getPersonalRankings().set(i, bordRankingForm.getPersonalRankings().get(i).replaceAll(",", ""));
+        }
+        log.debug("personal rankings after preprocessor = {}", bordRankingForm.getPersonalRankings());
+        BordRankingResult rankingResult = PersonalRanking.RankByBordMethod(bordRankingForm.getPersonalRankings());
+        model.addAttribute("ranking_result",rankingResult);
+        model.addAttribute("experts_count", bordRankingForm.getExpertsCount());
+        model.addAttribute("objects_count", bordRankingForm.getObjectsCount());
+        log.debug("EXIT");
+        return "bord_method_form";
+
+    }
+
+
 }
